64a9cd9f53d43b11555d187740cab7e600b22c2e
peggyyip <peggy.yip25@ycdsbk12.ca>
2024-04-16 05:06:19 -0400

    Final Sketch

--- Sketch.java
+++ Sketch.java
@@ -1,38 +1,134 @@
+/**
+ * Processing Task 6 - Exploring methods and parameters within those methods
+ * @author: P. Yip
+ */
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(112, 161, 160);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
+    // Defining variables for level 3
+    int intGrassLocationX;
+    int intGrassLocationY;
+
+    // Level 2 & 3 - house and tree respectively
+    noStroke();
+    drawHouse(50, 50);
+    drawHouse(250, 50);
+    drawHouse(150, 150);
+
+    drawTree(20, 70, 40, 50, 70);
+    drawTree(350, 70, 67, 60, 255);
+
+    drawHouse(250, 250);
+    drawHouse(50, 250);
+
+    drawTree(20, 270, 40, 70, 200);
+    drawTree(350, 210, 70, 100, 30);
+
+    // Level 4 - Grass
+    intGrassLocationX = grassLocationX(0);
+    intGrassLocationY = grassLocationY(0);
+    drawGrassBlades(intGrassLocationX, intGrassLocationY);
+
+    intGrassLocationX = grassLocationX(45);
+    intGrassLocationY = grassLocationY(-150);
+    drawGrassBlades(intGrassLocationX, intGrassLocationY);
     
+    intGrassLocationX = grassLocationX(-150);
+    intGrassLocationY = grassLocationY(50);
+    drawGrassBlades(intGrassLocationX, intGrassLocationY);
+
+    intGrassLocationX = grassLocationX(-350);
+    intGrassLocationY = grassLocationY(-150);
+    drawGrassBlades(intGrassLocationX, intGrassLocationY);
+
   }
 
   /**
-   * Description
+   * Description: Level Two, changing location of object (house).
    * 
-   * @param 
-   * @param 
-   * @return
-   * @author 
+   * @param: intHouseX; X coordinate
+   * @param: intHouseY; Y coordinate
+   * @return: void
+   * @author: P. Yip
    */
+    private void drawHouse(int intHouseX, int intHouseY) {
+      fill(252, 206, 78);
+      square(intHouseX, intHouseY, 100);
+      fill(212, 204, 184);
+      triangle(intHouseX + 100, intHouseY, intHouseX, intHouseY, intHouseX + 50, intHouseY - 50);
+      rect(intHouseX + 40, intHouseY + 50, 40, 50);
+      
+    }
+
+  /**
+   * Description: Level Three, adding additional paramaters to a new object (tree).
+   * 
+   * @param: treeX; X coordinate
+   * @param: treeY; Y coordinate
+   * @param: treeSize; trunk size
+   * @param: leafSize; size of leaves
+   * @param: treeColour; colour of leaves
+   * @return: void
+   * @author: P. Yip
+   */
+    private void drawTree(float treeX, float treeY, float treeSize, float leafSize, float treeColour) {
+      noStroke();
+      fill(133, 112, 110);
+      rect(treeX, treeY, treeSize, treeSize * 2);
+      fill(treeColour);
+      ellipse(treeX, treeY, leafSize * 3 / 2, leafSize);
+      ellipse(treeX + 20, treeY, leafSize, leafSize);
+      ellipse(treeX + 20, treeY - 25, leafSize, leafSize);
+      ellipse(treeX + 40, treeY - 15, leafSize, leafSize);
+      ellipse(treeX, treeY - 20, leafSize, leafSize);
+
+      }
+
+  /**
+   * Description: Level Four, using return method to obtain desired location of grass
+   * 
+   * @param: grassX; X coordinate
+   * @param: grassY; Y coordinate
+   * @return: int
+   * @author: P. Yip
+   */
+    
+   private int grassLocationX(int intGrassX) {
+      return intGrassX + 50;
+    }
+
+    private int grassLocationY(int intGrassY) {
+      return intGrassY + 150;
+    }
+
+    private void drawGrassBlades(int intGrassX, int intGrassY) {
+      fill(103, 148, 86);
+     for (int intGrassBlade = 1; intGrassBlade <= 10; intGrassBlade++) {
+      triangle(intGrassX, intGrassY, intGrassX + 20, intGrassY, intGrassX + 10, intGrassY - 20);
+        translate(10, 0);
+      }
+     translate(intGrassX, intGrassY);
+  
+    }
+      
+  }
+  
+
+ 
+    
+      
+  
+ 
 
   
   
-}
